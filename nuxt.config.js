f99d071cbd4ef8146d247135e4339df1776e78a5
ihsanbara94 <ihsanbatubara94@gmail.com>
2022-04-25 15:23:01 +0700

    adding route

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  target: "static",
+  router: {
+    base: "/assesment/",
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Pray-To-Earn",
